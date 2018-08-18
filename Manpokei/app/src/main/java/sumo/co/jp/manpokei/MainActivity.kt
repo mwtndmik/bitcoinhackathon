e4a35a08aee2fd70ebabaf7779ce22b6ca182fec
kingshow.200796 <kingshow.200796@gmail.com>
2018-08-18 18:25:52 +0900

    initial commit.

---
+++ Manpokei/app/src/main/java/sumo/co/jp/manpokei/MainActivity.kt
@@ -0,0 +1,12 @@
+package sumo.co.jp.manpokei
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
